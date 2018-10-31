3ae3631f9645340d2b8890f46bc89414250bece1
Lyle Classen <lylec@globalkinetic.com>
2018-10-31 16:00:12 +0200

    Added Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "jest": true,
+    "node": true
+  },
+  "extends": "airbnb",
+  "rules": {
+    "linebreak-style": ["off", process.env.NODE_ENV === 'production' ? "unix" : "windows"],
+    "function-paren-newline": ["error", "consistent"],
+    "jsx-a11y/anchor-is-valid": "off",
+    "jsx-a11y/label-has-for": "off",
+    "max-len": [2, 160],
+    "no-tabs": 0,
+    "object-curly-newline": ["error", { "consistent": true }],
+    "react/no-typos": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "react/destructuring-assignment": "off",
+    "no-multiple-empty-lines": "off",
+    "implicit-arrow-linebreak": "off",
+    "operator-linebreak": "off",
+  },
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "allowImportExportEverywhere": true
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "paths": ["node_modules", "src"]
+      }
+    }
+  }
+};
+
