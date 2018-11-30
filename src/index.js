17156ffce4a56831e2941c8adb3a568f4506ffed
Lyle Classen <lylec@globalkinetic.com>
2018-11-30 14:36:28 +0200

    Added WorkBox SW

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './app';
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/workbox-sw.js');
+  });
+}
